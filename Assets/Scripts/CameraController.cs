7dd412153e6b3edc919a29fa7ebc2f85aac88240
Joris Menke <jorismenke@gmail.com>
2019-03-20 22:43:43 +0100

    Initial commit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+	//public variables
+	public float minimumX = -60f;
+	public float maximumX = 60f;
+	public float minimumY = -360f;
+	public float maximumY = 360f;
+	public float sensitivityX = 15f;
+	public float sensitivityY = 15f;
+	public Camera cam;
+	public GameObject player;
+
+	//private variables
+	private float rotationY = 0f;
+	private float rotationX = 0f;
+	private Vector3 offset;
+
+
+	void Update() {
+		rotationY += Input.GetAxis("Mouse X") * sensitivityY;
+		rotationX += Input.GetAxis("Mouse Y") * sensitivityX;
+
+		rotationX = Mathf.Clamp(rotationX, minimumX, maximumX);
+
+		transform.localEulerAngles = new Vector3(0, rotationY, 0);
+		cam.transform.localEulerAngles = new Vector3(-rotationX, rotationY, 0);
+		cam.transform.position = player.transform.position + offset;
+	}
+
+	void Start(){
+		Cursor.lockState = CursorLockMode.Locked;
+
+		if (Input.GetKey(KeyCode.Escape))
+		{
+			Cursor.lockState = CursorLockMode.None;
+			Cursor.visible = true;
+		}
+		
+		offset = cam.transform.position - player.transform.position;
+	}
+
+
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	//Public variables
+	public float walkSpeed;
+
+	//Private variables
+	Rigidbody rb;
+	Vector3 moveDirection;
+
+	void Awake()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+
+//Non-Physics steps
+	void Update()
+	{
+		//Get directional input from the user
+		float horizontalMovement = Input.GetAxisRaw("Horizontal");
+		float verticalMovement = Input.GetAxisRaw("Vertical");
+
+		moveDirection = (horizontalMovement * transform.right + verticalMovement * transform.forward).normalized;
+	}
+
+//Physics steps
+	void FixedUpdate()
+	{
+		Move();
+	}
+
+	void Move()
+	{
+		//Rigidbody.velocity is a method which takes a Vector 3
+		//and controls the speed and direction of the GameObject
+		Vector3 yVelFix = new Vector3(0, rb.velocity.y, 0);
+		rb.velocity = moveDirection * walkSpeed * Time.deltaTime;
+		rb.velocity += yVelFix;
+	}
+}
